962b8b5832c09208885e491684c587fc1374b0ec
DipakKumar6-code <126050058+DipakKumar6-code@users.noreply.github.com>
2024-08-13 16:45:19 -0400

    Question 6 Complete

--- comp1011FinalExamSummer2024/src/main/java/com/example/comp1011finalexamsummer2024/ParseJson/parseJsonCustomer.java
+++ comp1011FinalExamSummer2024/src/main/java/com/example/comp1011finalexamsummer2024/ParseJson/parseJsonCustomer.java
@@ -1,43 +1,78 @@
-package com.example.comp1011finalexamsummer2024.ParseJson;
+//package com.example.comp1011finalexamsummer2024.ParseJson;
+//
+//// Author: Dipak Bhul
+//// Student Number: 200564268
+//
+//import com.example.comp1011finalexamsummer2024.Model.Customer;
+//import com.google.gson.Gson;
+//import com.google.gson.reflect.TypeToken;
+//
+//import java.io.FileReader;
+//import java.io.IOException;
+//import java.lang.reflect.Type;
+//import java.util.ArrayList;
+//
+//public class parseJsonCustomer {
+//
+//    // Method to parse JSON file into ArrayList of Customer objects
+//    public static ArrayList<Customer> loadCustomerData(String filePath) {
+//        Gson gson = new Gson();
+//        ArrayList<Customer> customers = new ArrayList<>();
+//
+//        try (FileReader reader = new FileReader(filePath)) {
+//            Type customerListType = new TypeToken<ArrayList<Customer>>() {}.getType();
+//            customers = gson.fromJson(reader, customerListType);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        return customers;
+//    }
+//
+//    public static void main(String[] args) {
+//        // Assuming the JSON file is in the resources directory
+//        String filePath = "/com/example/comp1011finalexamsummer2024/customers.json";
+//        ArrayList<Customer> customers = loadCustomerData(filePath);
+//
+//        // Print out customers for verification
+//        for (Customer customer : customers) {
+//            System.out.println(customer.getFirstName() + " " + customer.getLastName());
+//        }
+//    }
+//}
+//
 
-// Author: Dipak Bhul
-// Student Number: 200564268
+package com.example.comp1011finalexamsummer2024.ParseJson;
 
 import com.example.comp1011finalexamsummer2024.Model.Customer;
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import com.google.gson.reflect.TypeToken;
 
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.InputStreamReader;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 public class parseJsonCustomer {
 
     // Method to parse JSON file into ArrayList of Customer objects
-    public static ArrayList<Customer> parseCustomersJson(String filePath) {
+    public static ArrayList<Customer> loadCustomerData(String filePath) {
         Gson gson = new Gson();
         ArrayList<Customer> customers = new ArrayList<>();
 
-        try (FileReader reader = new FileReader(filePath)) {
+        try (InputStreamReader reader = new InputStreamReader(parseJsonCustomer.class.getResourceAsStream(filePath))) {
+            JsonObject jsonObject = gson.fromJson(reader, JsonObject.class);
             Type customerListType = new TypeToken<ArrayList<Customer>>() {}.getType();
-            customers = gson.fromJson(reader, customerListType);
-        } catch (IOException e) {
+            customers = gson.fromJson(jsonObject.get("customers"), customerListType);
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
         return customers;
     }
-
-    public static void main(String[] args) {
-        // Assuming the JSON file is in the resources directory
-        String filePath = "src/main/resources/customers.json";
-        ArrayList<Customer> customers = parseCustomersJson(filePath);
-
-        // Print out customers for verification
-        for (Customer customer : customers) {
-            System.out.println(customer.getFirstName() + " " + customer.getLastName());
-        }
-    }
 }
+
+
+
+
 
--- comp1011FinalExamSummer2024/src/main/java/module-info.java
+++ comp1011FinalExamSummer2024/src/main/java/module-info.java
@@ -1,10 +1,31 @@
+//module com.example.comp1011finalexamsummer2024 {
+//    requires javafx.controls;
+//    requires javafx.fxml;
+//    requires com.google.gson;
+//    requires java.net.http;
+//
+//
+//    opens com.example.comp1011finalexamsummer2024 to javafx.fxml, com.google.gson;
+//    exports com.example.comp1011finalexamsummer2024;
+//    exports com.example.comp1011finalexamsummer2024.Model;
+//    exports com.example.comp1011finalexamsummer2024.Controllers;
+//    exports com.example.comp1011finalexamsummer2024.ParseJson;
+//}
+
 module com.example.comp1011finalexamsummer2024 {
     requires javafx.controls;
     requires javafx.fxml;
     requires com.google.gson;
     requires java.net.http;
 
+    // Open packages to JavaFX and Gson for reflection
+    opens com.example.comp1011finalexamsummer2024.Controllers to javafx.fxml;
+    opens com.example.comp1011finalexamsummer2024.Model to com.google.gson;
+    opens com.example.comp1011finalexamsummer2024.ParseJson to com.google.gson;
 
-    opens com.example.comp1011finalexamsummer2024 to javafx.fxml, com.google.gson;
+    // Export packages for other modules to use
     exports com.example.comp1011finalexamsummer2024;
+    exports com.example.comp1011finalexamsummer2024.Model;
+    exports com.example.comp1011finalexamsummer2024.Controllers;
+    exports com.example.comp1011finalexamsummer2024.ParseJson;
 }
